f8078838c2dc34dc4fa84269053523e6c4ef3e54
Pete Kreppein <pmkreppein@gmail.com>
2018-11-27 16:32:59 -0500

    First commit, project setup.

---
+++ lib/crypto/tracker.rb
@@ -0,0 +1,7 @@
+require "crypto/tracker/version"
+
+module Crypto
+  module Tracker
+    # Your code goes here...
+  end
+end
---
+++ lib/crypto/tracker/version.rb
@@ -0,0 +1,5 @@
+module Crypto
+  module Tracker
+    VERSION = "0.1.0"
+  end
+end
