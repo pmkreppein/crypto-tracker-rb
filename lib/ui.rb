0f743be84bbc52741746e163b8caf21f7164d7e4
Pete Kreppein <pmkreppein@gmail.com>
2018-11-28 13:22:20 -0500

    Final Fixes, ASCII ART :D

--- lib/ui.rb
+++ lib/ui.rb
@@ -8,19 +8,31 @@ class UI
       @formatter = Formatter.new
     end
 
-
+    def ascii()
+      puts <<-HEREDOC
+      .o88b. d8888b. db    db d8888b. d888888b  .d88b.       d888888b d8888b.  .d8b.   .o88b. db   dD d88888b d8888b.
+     d8P  Y8 88  `8D `8b  d8' 88  `8D `~~88~~' .8P  Y8.      `~~88~~' 88  `8D d8' `8b d8P  Y8 88 ,8P' 88'     88  `8D
+     8P      88oobY'  `8bd8'  88oodD'    88    88    88         88    88oobY' 88ooo88 8P      88,8P   88ooooo 88oobY'
+     8b      88`8b      88    88~~~      88    88    88         88    88`8b   88~~~88 8b      88`8b   88~~~~~ 88`8b
+     Y8b  d8 88 `88.    88    88         88    `8b  d8'         88    88 `88. 88   88 Y8b  d8 88 `88. 88.     88 `88.
+      `Y88P' 88   YD    YP    88         YP     `Y88P'          YP    88   YD YP   YP  `Y88P' YP   YD Y88888P 88   YD
+      HEREDOC
+    end
     def ui_loop()
 
+        puts "#{ascii}"
         puts <<-HEREDOC
         ______________________________________________________________________________
             Welcome to the Crypto Tracker CLI App.
             Please Select from the below menu:
             [S]imple View: Bitcoin, Ethereum, and Litecoin prices in USD.
             [D]etailed View: Bitcoin, Ethereum, and Litcoin prices in USD, GBP, EUR.
+            [R]efresh: Refresh the current price data.
             [Q]uit: Exit the app
             Please make a selection....
         HEREDOC
     end
+
 
 
     def run()
@@ -39,6 +51,7 @@ class UI
 
               when "S", "s"
                 system "clear"
+                puts "#{ascii}"
                 puts "#{@formatter.simple(@networking.last_report)}"
                 puts "Push any key to return to Main Menu."
                 input = gets.chomp
@@ -46,6 +59,7 @@ class UI
 
               when "D", "d"
                 system "clear"
+                puts "#{ascii}"
                 puts "#{@formatter.advanced(@networking.last_report)}"
                 puts "Push any key to return to Main Menu."
                 input = gets.chomp
@@ -53,16 +67,20 @@ class UI
 
               when "r", "R"
                 system "clear"
+                puts "#{ascii}"
                 puts "Refreshing prices..."
-                sleep(1)
                 @networking.refresh_prices
                 puts "Prices Refreshed Successfully!"
+                sleep(1)
 
               else
+                system "clear"
                 puts "Sorry, that wasn't a choice I recognize.  Try again?"
                 sleep(2)
                 self.run()
             end
+
+
           end
 
 
