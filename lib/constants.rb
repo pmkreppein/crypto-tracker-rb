84e1117d712b6f4175ae6641787da5bd7416dd6b
Pete Kreppein <pmkreppein@gmail.com>
2018-12-05 22:29:11 -0500

    See commit note in commit-notes/12-5-19.md

---
+++ lib/constants.rb
@@ -0,0 +1,48 @@
+class Constants
+    def self.main_menu()
+        puts <<-HEREDOC
+        ______________________________________________________________________________
+            Welcome to the Crypto Tracker CLI App.
+            Please Select from the below menu:
+            [S]imple View: Bitcoin, Ethereum, and Litecoin prices in USD.
+            [D]etailed View: Bitcoin, Ethereum, and Litcoin prices in USD, GBP, EUR.
+            [R]efresh: Refresh the current price data.
+            [Q]uit: Exit the app
+            Please type a selection and press 'Return' or 'Enter'....
+        HEREDOC
+    end
+
+    def self.ascii()
+        puts <<-HEREDOC
+        .o88b. d8888b. db    db d8888b. d888888b  .d88b.       d888888b d8888b.  .d8b.   .o88b. db   dD d88888b d8888b.
+       d8P  Y8 88  `8D `8b  d8' 88  `8D `~~88~~' .8P  Y8.      `~~88~~' 88  `8D d8' `8b d8P  Y8 88 ,8P' 88'     88  `8D
+       8P      88oobY'  `8bd8'  88oodD'    88    88    88         88    88oobY' 88ooo88 8P      88,8P   88ooooo 88oobY'
+       8b      88`8b      88    88~~~      88    88    88         88    88`8b   88~~~88 8b      88`8b   88~~~~~ 88`8b
+       Y8b  d8 88 `88.    88    88         88    `8b  d8'         88    88 `88. 88   88 Y8b  d8 88 `88. 88.     88 `88.
+        `Y88P' 88   YD    YP    88         YP     `Y88P'          YP    88   YD YP   YP  `Y88P' YP   YD Y88888P 88   YD
+        
+        HEREDOC
+    end
+
+    def self.detailed_report(btc_data, ltc_data, eth_data)
+        puts <<-HEREDOC
+        Detailed Price Report
+          _________________
+              Bitcoin
+                ** USD: #{btc_data.USD}
+                ** GBP: #{btc_data.GBP}
+                ** EUR: #{btc_data.EUR}
+          _________________
+              Litecoin
+                ** USD: #{ltc_data.USD}
+                ** GBP: #{ltc_data.GBP}
+                ** EUR: #{ltc_data.EUR}
+          _________________
+              Ethereum:
+                ** USD: #{eth_data.USD}
+                ** GBP: #{eth_data.GBP}
+                ** EUR: #{eth_data.EUR}
+    HEREDOC
+    end
+
+end
--- lib/ui.rb
+++ lib/ui.rb
@@ -2,131 +2,104 @@
 
 class UI
   #Spin up Networking and Formatter Classes
-    def initialize()
-      @networking = Networking.new
-      @formatter = Formatter.new
-      data = @networking.prices
-      create_coins(data)
-    end
-#ASCII Art Block
-    def ascii()
-      puts <<-HEREDOC
-      .o88b. d8888b. db    db d8888b. d888888b  .d88b.       d888888b d8888b.  .d8b.   .o88b. db   dD d88888b d8888b.
-     d8P  Y8 88  `8D `8b  d8' 88  `8D `~~88~~' .8P  Y8.      `~~88~~' 88  `8D d8' `8b d8P  Y8 88 ,8P' 88'     88  `8D
-     8P      88oobY'  `8bd8'  88oodD'    88    88    88         88    88oobY' 88ooo88 8P      88,8P   88ooooo 88oobY'
-     8b      88`8b      88    88~~~      88    88    88         88    88`8b   88~~~88 8b      88`8b   88~~~~~ 88`8b
-     Y8b  d8 88 `88.    88    88         88    `8b  d8'         88    88 `88. 88   88 Y8b  d8 88 `88. 88.     88 `88.
-      `Y88P' 88   YD    YP    88         YP     `Y88P'          YP    88   YD YP   YP  `Y88P' YP   YD Y88888P 88   YD
-      HEREDOC
-    end
-    
-    def startup()
-        system "clear"
-        puts "Welcome to Crypto Tracker!  Bear with us while we set things up!"
-        sleep(1)
-        puts "Done!"
-        sleep(1)
-        system "clear"
-        run()
-    end
+  def initialize()
+    @networking = Networking.new
+    data = @networking.prices
+    create_coins(data)
+  end
+  
+  #Loop for startup screen 
+  def startup()
+    system "clear"
+    puts "Welcome to Crypto Tracker!  Bear with us while we set things up!"
+    sleep(1)
+    puts "Done!"
+    sleep(1)
+    system "clear"
+    run()
+  end
 
 #UI Menu Block
-    def ui_loop()
+  def ui_loop()
+    Constants.ascii()
+    Constants.main_menu()
+  end
 
-        puts "#{ascii}"
-        puts <<-HEREDOC
-        ______________________________________________________________________________
-            Welcome to the Crypto Tracker CLI App.
-            Please Select from the below menu:
-            [S]imple View: Bitcoin, Ethereum, and Litecoin prices in USD.
-            [D]etailed View: Bitcoin, Ethereum, and Litcoin prices in USD, GBP, EUR.
-            [R]efresh: Refresh the current price data.
-            [Q]uit: Exit the app
-            Please type a selection and press 'Return' or 'Enter'....
-        HEREDOC
-    end
-
-  def run_refresh()
+#Refresh Prices Loop  
+def run_refresh()
     @networking.refresh_prices
     data = @networking.prices
     update_coins(data)
-  end
+end
 
-  def create_coins(data)
+#Create Coin Instances
+def create_coins(data)
     @bitcoin = Coin.new(data.BTC)
     @ethereum = Coin.new(data.ETH)
     @litecoin = Coin.new(data.LTC)
-  end
+end
 
-  def update_coins(data)
+#Update Coin prices
+def update_coins(data)
     @bitcoin.update(data.BTC)
-    @ethereum.update(data.BTC)
-    @litecoin.update(data.BTC)
- end
+    @ethereum.update(data.ETH)
+    @litecoin.update(data.LTC)
+end
 
 
 
 #Core App Loop
-    def run()
-        while true
-            system "clear"
-            ui_loop()
-            input = gets.chomp
+def run()
+  while true
+    system "clear"
+    ui_loop()
+    input = gets.chomp
 #Evaluate Choices
-            case input
-              when "Q" , "q"
-                system "clear"
-                puts "Thanks for using Crypto Tracker!  Please wait for program to close."
-                sleep(1)
-                system "clear"
-                abort("Crypto Tracker closed successfully")
+    case input
+      when "Q" , "q"
+        system "clear"
+        puts "Thanks for using Crypto Tracker!  Please wait for program to close."
+        sleep(1)
+        system "clear"
+        abort("Crypto Tracker closed successfully")
 #Simple Price Layout
-              when "S", "s"
-                system "clear"
-                puts "#{ascii}"
-                puts <<-HEREDOC
-                        Top Cryptos in USD:
-                        -- Bitcoin: #{@bitcoin.usd}
-                        -- Ethereum: #{@ethereum.usd}
-                        -- Litecoin: #{@litecoin.usd}
-                    HEREDOC
-                puts "Push any key to return to Main Menu."
-                input = gets.chomp
-                sleep(1) until input
+      when "S", "s"
+        system "clear"
+        Constants.ascii()
+        puts <<-HEREDOC
+                Top Cryptos in USD:
+                -- Bitcoin: #{@bitcoin.usd}
+                -- Ethereum: #{@ethereum.usd}
+                -- Litecoin: #{@litecoin.usd}
+            HEREDOC
+        puts "Push any key to return to Main Menu."
+        input = gets.chomp
+        sleep(1) until input
 #Detailed Price Layout
-              when "D", "d"
-                system "clear"
-                puts "#{ascii}"
-                puts "#{@formatter.advanced(@networking.last_report)}"
-                puts "Push any key to return to Main Menu."
-                input = gets.chomp
-                sleep(1) until input
+      when "D", "d"
+        system "clear"
+        Constants.ascii()
+        Constants.detailed_report(@bitcoin.raw_data, @litecoin.raw_data, @ethereum.raw_data)
+        puts "Push any key to return to Main Menu."
+        input = gets.chomp
+        sleep(1) until input
 #Refresh Prices
-              when "r", "R"
-                system "clear"
-                puts "#{ascii}"
-                puts "Refreshing prices..."
-                self.run_refresh()
-                puts "Prices Refreshed Successfully!"
-                sleep(1)
+      when "r", "R"
+        system "clear"
+        Constants.ascii()
+        puts "Refreshing prices..."
+        self.run_refresh()
+        puts "Prices Refreshed Successfully!"
+        sleep(1)
 #Unknown Input
-              else
-                system "clear"
-                puts "¯\\_(ツ)_/¯"
-                puts "Sorry, that wasn't a choice I recognize.  Try again?"
-                sleep(3)
-                self.run()
-            end
+      else
+        system "clear"
+        puts "¯\\_(ツ)_/¯"
+        puts "Sorry, that wasn't a choice I recognize.  Try again?"
+        sleep(3)
+        self.run()
+        end ##Else End  
+    end ##Case End
+  end ##Run() End
 
-
-          end
-
-
-
-end
-
-
-
-
-
-end
+end ##Class end
